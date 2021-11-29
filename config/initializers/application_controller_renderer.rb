df2c8c5f1bec70a857b6061b173639c080e8564e
ishizukamaaya <maayajas@gmail.com>
2021-11-29 08:36:19 +0000

    create

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '88060f7ecdb628f091e875731c60ecd0f8e5fd844dfde47fc5c8ee6eec8ebc0ea4f4b0db47e4bd31a5e5233f7728ecd4b769c043b36e1e98972c2dfcaa621fc5'
