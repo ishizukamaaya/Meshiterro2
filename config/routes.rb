787411c70dab8775d4a7b03c2d7e2eb018317d18
ishizukamaaya <maayajas@gmail.com>
2021-12-10 12:53:00 +0000

    about-create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about'
 
   resources :post_images do
     resources :post_comments, only:[:create, :destroy]
