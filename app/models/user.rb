cc5fe965e57bb565996acb0a7c22be618fc0321a
ishizukamaaya <maayajas@gmail.com>
2021-12-13 05:10:11 +0000

    follow-create

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,29 @@ class User < ApplicationRecord
   has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy #フォローしてる人の取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy #フォローされている人の取得
+  has_many :following_user, through: :follower, source: :followed #自分がフォローしてる人
+  has_many :follower_user, through: :followed, source: :follower　#自分をフォローしてる人
+
   attachment :profile_image
 
   validates :name, presence: true
-  validates :profile_image,presence: true
+
+  #フォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  #フォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  #フォロー確認
+  def following(user)
+    following_user.include?(user)
+  end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_012906) do
+ActiveRecord::Schema.define(version: 2021_12_13_043339) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2021_12_02_012906) do
     t.string "image_id"
     t.text "caption"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
